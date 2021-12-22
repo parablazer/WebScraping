e41040e3255931349d64574afc292fd1b1ed62c0
parablazer <parablazer@yahoo.com>
2021-12-21 21:37:38 -0600

    Requets_html testing

---
+++ requests-html.py
@@ -0,0 +1,22 @@
+from requests_html import HTMLSession, HTML
+
+session = HTMLSession()
+r = session.get('https://parablazer.github.io/')
+
+articles = r.html.find('article')
+#title = article.find('h3', first=True).text
+#print(title)
+
+
+
+
+for article in articles:
+    title = article.find('h3', first=True).text
+    desc = article.find('p', first=True).text
+    link = article.find('a', first=True)
+    print(title)
+    print(desc)
+    print(link.attrs['href'])
+    print()
+
+
