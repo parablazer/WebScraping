1caa5066c07e56c5dd7d1043cd905b98ad06f8e0
parablazer <parablazer@yahoo.com>
2021-12-21 21:54:54 -0600

    Requets_html testing 2

--- requests-html.py
+++ requests-html.py
@@ -1,5 +1,9 @@
 from requests_html import HTMLSession, HTML
+import csv
 
+csv_file = open('page_scrape.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['title', 'desc', 'link'])
 session = HTMLSession()
 r = session.get('https://parablazer.github.io/')
 
@@ -14,9 +18,13 @@ for article in articles:
     title = article.find('h3', first=True).text
     desc = article.find('p', first=True).text
     link = article.find('a', first=True)
+
     print(title)
     print(desc)
     print(link.attrs['href'])
     print()
+
+    csv_writer.writerow([title, desc, link])
+csv_file.close()
 
 
