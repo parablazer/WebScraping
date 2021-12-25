6f1522d705ae691eac714b6ce2dcb499dc09bd02
parablazer <parablazer@yahoo.com>
2021-12-25 16:48:03 -0600

    Requets_html testing 2

--- requests-html.py
+++ requests-html.py
@@ -1,30 +1,51 @@
 from requests_html import HTMLSession, HTML
 import csv
+html = HTML
+#csv_file = open('page_scrape.csv', 'w')
+#csv_writer = csv.writer(csv_file)
+#csv_writer.writerow(['title', 'desc', 'link'])
 
-csv_file = open('page_scrape.csv', 'w')
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['title', 'desc', 'link'])
-session = HTMLSession()
-r = session.get('https://parablazer.github.io/')
+s = HTMLSession()
+url = 'https://duckduckgo.com/?q=cnn&t=h_&ia=web'
 
-articles = r.html.find('article')
+r = s.get(url)
+
+r.html.render(sleep=1)
+
+links = r.html.xpath('//*[@id="links"]', first=True)
+print(links)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#articles = r.html.find('article')
 #title = article.find('h3', first=True).text
 #print(title)
 
 
 
-
-for article in articles:
-    title = article.find('h3', first=True).text
-    desc = article.find('p', first=True).text
-    link = article.find('a', first=True)
-
-    print(title)
-    print(desc)
-    print(link.attrs['href'])
-    print()
-
-    csv_writer.writerow([title, desc, link])
-csv_file.close()
+#for article in articles:
+ #   title = article.find('h3', first=True).text
+#    desc = article.find('p', first=True).text
+#    link = article.find('a', first=True)
+#
+#    print(title)
+#    print(desc)
+#    print(link.attrs['href'])
+#    print()
+#
+#    csv_writer.writerow([title, desc, link])
+#csv_file.close()
 
 
